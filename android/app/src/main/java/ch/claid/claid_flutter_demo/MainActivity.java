ec69894735b1eb20384daec5c2c16b162f90de59
Patrick Langer <patrick.langer97@gmx.de>
2024-06-27 17:02:54 +0200

    Adding code.

---
+++ android/app/src/main/java/ch/claid/claid_flutter_demo/MainActivity.java
@@ -0,0 +1,9 @@
+package ch.claid.claid_flutter_demo;
+
+import adamma.c4dhi.claid.CLAIDFlutterActivity;
+
+public class MainActivity extends CLAIDFlutterActivity
+{
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:claid/module/module_factory.dart';
+import 'package:claid/ui/CLAIDView.dart';
+import 'package:claid/CLAID.dart';
+
+import 'package:flutter/material.dart';
+
+import 'TestStreamModule.dart';
+import 'TestStreamView.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CLAID App',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+{
+  bool claidStarted = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    CLAID.registerModule("TestStreamModule", TestStreamModule.new);
+    CLAID.registerViewClassForModule("TestStreamModule", TestStreamView.new);
+
+    CLAID.startInForeground(
+        configFilePath: "assets://flutter_assets/assets/claid_test.json",
+        hostId: "Smartphone",
+        userId: "test_user",
+        deviceId: "test_id",
+        specialPermissionsConfig: CLAIDSpecialPermissionsConfig.regularConfig(),
+        claidPackages: []
+    ).then((value) => setState(() {
+      claidStarted = true;
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return  claidStarted ?
+      const CLAIDView(title: 'My CLAID App') :
+      const CircularProgressIndicator();
+  }
+}
